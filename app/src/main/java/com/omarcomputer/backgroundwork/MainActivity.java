3a4d9f4b8b59d9de4882918f38675c0b10d93c53
Omar OUGHZAL <omar.oughzal@gmail.com>
2023-11-01 22:05:15 +0100

    Initial commit

---
+++ app/src/main/java/com/omarcomputer/backgroundwork/MainActivity.java
@@ -0,0 +1,14 @@
+package com.omarcomputer.backgroundwork;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
