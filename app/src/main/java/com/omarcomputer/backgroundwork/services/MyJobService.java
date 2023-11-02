3d2a9972a9490bc7cf2be7a9bb24de237f37096e
Omar OUGHZAL <omar.oughzal@gmail.com>
2023-11-02 21:48:55 +0100

    Initial commit

--- app/src/main/java/com/omarcomputer/backgroundwork/services/MyJobService.java
+++ app/src/main/java/com/omarcomputer/backgroundwork/services/MyJobService.java
@@ -1,11 +1,17 @@
 package com.omarcomputer.backgroundwork.services;
 
+
+
 import android.annotation.SuppressLint;
 import android.app.job.JobParameters;
 import android.app.job.JobService;
+import android.util.Log;
 
 
+
+@SuppressLint("SpecifyJobSchedulerIdRange")
 public class MyJobService extends JobService {
+    private static String TAG ="MyJobServiceTag";
     public boolean jobCancelled=false;
     @Override
     public boolean onStartJob(JobParameters params) {
@@ -13,7 +19,26 @@ public class MyJobService extends JobService {
         return false;
     }
 
+
     private void doBackground(JobParameters params) {
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++){
+                    Log.d(TAG, "run : "+i);
+                    if(jobCancelled)
+                        return;
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                Log.d(TAG, "job terminé");
+                jobFinished(params, false);
+            }
+        }).start();
     }
 
     @Override
