0ce2c43ea1d9da54f248fc0b7a8aa72cfde35c3c
Omar OUGHZAL <omar.oughzal@gmail.com>
2023-11-08 22:16:34 +0100

    Initial commit

--- app/src/main/java/com/omarcomputer/backgroundwork/MainActivity.java
+++ app/src/main/java/com/omarcomputer/backgroundwork/MainActivity.java
@@ -1,10 +1,18 @@
 package com.omarcomputer.backgroundwork;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 import androidx.work.PeriodicWorkRequest;
 import androidx.work.WorkManager;
 import androidx.work.WorkRequest;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -22,21 +30,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         workManager = WorkManager.getInstance(getApplicationContext());
-        workRequest = new PeriodicWorkRequest.Builder(MyWorker.class, 2,TimeUnit.MINUTES ).build();
+        workRequest = new PeriodicWorkRequest.Builder(MyWorker.class, 15,TimeUnit.MINUTES)
+                .setInitialDelay(1,TimeUnit.MINUTES)
+                .build();
         btnStart = findViewById(R.id.btnStart);
-        btnStart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startWork();
-            }
-        });
+        btnStart.setOnClickListener(v -> startWork());
         btnStop = findViewById(R.id.btnStop);
-        btnStop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                stopWork();
-            }
-        });
+        btnStop.setOnClickListener(v -> stopWork());
 
     }
 
@@ -46,4 +46,5 @@ public class MainActivity extends AppCompatActivity {
     private void stopWork(){
         workManager.cancelWorkById(workRequest.getId());
     }
+
 }
--- app/src/main/java/com/omarcomputer/backgroundwork/services/MyWorker.java
+++ app/src/main/java/com/omarcomputer/backgroundwork/services/MyWorker.java
@@ -1,11 +1,21 @@
 package com.omarcomputer.backgroundwork.services;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
+
+import com.omarcomputer.backgroundwork.MainActivity;
+import com.omarcomputer.backgroundwork.R;
 
 public class MyWorker extends Worker {
     private static final String TAG = "FileReadWork";
@@ -25,9 +35,38 @@ public class MyWorker extends Worker {
     @NonNull
     @Override
     public Result doWork() {
-            Log.i("MyWorker", "Work done");
+            postNottification();
+            Log.i("MyWorkerTAG", "Work done");
         return Result.success();
     }
 
+    private void postNottification(){
+        String channelId = "CHANNEL_ID_NOTIFICATION";
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(),channelId );
+        builder.setSmallIcon(R.drawable.ic_notification);
+        builder.setContentTitle("DEV201");
+        builder.setContentText("Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus doloremque cumque eveniet temporibus");
+        builder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        builder.setAutoCancel(true);
 
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_FORWARD_RESULT);
+        intent.putExtra("title","From notification");
+        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, intent, PendingIntent.FLAG_IMMUTABLE);
+        builder.setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Mon canal de notification";
+            String description = "Description du canal de notification";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(channelId, name, importance);
+            channel.setDescription(description);
+            channel.setLightColor(Color.RED);
+            channel.enableVibration(true);
+            notificationManager.createNotificationChannel(channel);
+        }
+        notificationManager.notify(0, builder.build());
+    }
 }
